919e60239d94c67c59b8c7ccf9db8f6e616bf38d
nautbek <nautbek@gmail.com>
2024-09-04 06:27:22 +0300

    Version 1.0.5.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.trainingdiary"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
